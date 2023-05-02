51a281d6ebf10b1e077885c15b373c370bb5171a
Sumit Jangid <sumitjangid@Sumits-Air.attlocal.net>
2023-05-01 22:10:07 -0700

    experience file updated for mobile

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -49,7 +49,7 @@ const ExperienceCard = ({ experience }) => {
 const Experience = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div>
         <p className={`${styles.sectionSubText}`}>What I have done so far</p>
         <h2 className={`${styles.sectionHeadText}`}>Work Experience.</h2>
       </motion.div>
