dcfc63893a4d909b66f1103867ebfcef2aa48dfd
Sumit Jangid <sumitjangid@Sumits-Air.attlocal.net>
2023-05-01 20:10:31 -0700

    updated file path for gltf

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
 
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("../desktop_pc/scene.gltf");
 
   return (
     <mesh>
