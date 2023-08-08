90d4cf1581db4d84535b9fb38eb3dbe8db9d907c
Sumit Jangid <sumitjangid@Sumits-Air.attlocal.net>
2023-08-07 20:07:57 -0700

    updated assets indexjs file

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import travismathew from "./company/travismathew.png";
+import experis from "./company/experis.png";
+import fortyeightws from "./company/fortyeightws.png";
+import threelinedesign from "./company/threelinedesign.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +52,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  travismathew,
+  experis,
+  fortyeightws,
+  threelinedesign,
   carrent,
   jobit,
   tripguide,
